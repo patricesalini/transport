6b5436c8ac4d44a277aca775f76731ff6e6e03ed
patintosh <patrice.salini@wanadoo.fr>
2026-02-15 19:01:46 +0100

    Create search.js

---
+++ search.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Résultats de recherche</title>
+</head>
+<body>
+
+<h1>Résultats</h1>
+
+<input id="searchBox" type="text" placeholder="Tapez votre recherche…">
+<button id="searchButton">Rechercher</button>
+
+<div id="results"></div>
+
+<script src="search.js"></script>
+
+</body>
+</html>
