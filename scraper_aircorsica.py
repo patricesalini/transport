e7ac0c2b1576d4ec62efd4e73adcfccf6d819e39
patintosh <patrice.salini@wanadoo.fr>
2026-07-30 15:16:56 +0200

    Update scraper_aircorsica.py

--- scraper_aircorsica.py
+++ scraper_aircorsica.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, AttributeError
+from selenium.common.exceptions import TimeoutException
 
 ROUTES = [
     ("ORY", "AJA"),
