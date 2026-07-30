1341911e01443dee9cb94fdff79959f6d3f9b6b9
patintosh <patrice.salini@wanadoo.fr>
2026-07-30 14:58:29 +0200

    Fix import statement for WebDriverWait

--- scraper_aircorsica.py
+++ scraper_aircorsica.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 ROUTES = [
